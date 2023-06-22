8fad5d39bd690e50bdd0ec30ebb088d3b62633a1
Elif Eren <119631570+elifEren0@users.noreply.github.com>
2023-06-23 01:04:11 +0300

    Add files via upload

---
+++ FSC/FS.m
@@ -0,0 +1,23 @@
+load('F16_LookupTables.mat')
+
+%PROPERTIES
+mass = 9300; % kg
+Ixx = 12875; % kgm^2
+Iyy = 75674; % kgm^2
+Izz = 85552; % kgm^2
+Ixz = 1331; % kgm^2
+c = 3.45; % m
+S = 27.88; % m^2
+b = 9.144; % m
+
+g = 9.80665; % gravity
+dHT = 0;
+deltalef= 0;
+deltaSB = 0;
+delta_e = 0;
+delta_a = 0;
+delta_r = 0;
+
+Pc = 50;
+
+
